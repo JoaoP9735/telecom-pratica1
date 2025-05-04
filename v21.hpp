277fb7ff70f3a450700cf1cfc89b1d99b34d4610
joao_andrade <106917014+JoaoP9735@users.noreply.github.com>
2025-05-04 18:52:16 -0300

    Update v21.hpp

--- v21.hpp
+++ v21.hpp
@@ -2,37 +2,40 @@
 #define V21_HPP
 
 #include <functional>
+#include <deque>
 #include "config.hpp"
-#include <cmath>
-class V21_RX {
+
+class V21_RX
+{
 public:
-    V21_RX(float omega_mark, float omega_space,
-           std::function<void(const unsigned int *, unsigned int)> get_digital_samples)
-        : omega_mark(omega_mark),
-          omega_space(omega_space),
-          get_digital_samples(get_digital_samples),
-          freq_mark(omega_mark / (2 * M_PI)),
-          freq_space(omega_space / (2 * M_PI)) {}
-
+    V21_RX(float omega_mark, float omega_space, std::function<void(const unsigned int *, unsigned int)> get_digital_samples);
     void demodulate(const float *in_analog_samples, unsigned int n);
-
 private:
     float omega_mark, omega_space;
+    float rl_cos_space, rl_sin_space, r_cos_space, r_sin_space; 
+    float rl_cos_mark, rl_sin_mark, r_cos_mark, r_sin_mark; 
+    float lp_numerator[3];
+    float lp_denominator[3];
+
+    std::deque<float> sample_buffer;
+    float vspace_r_buffer, vspace_i_buffer;
+    float vmark_r_buffer, vmark_i_buffer;
+    float raw_decision_buffer[2];
+    float filtered_decision_buffer[2];
+
+    enum {
+        IDLE,
+        CARRIER_DETECTED
+    } state;
+    unsigned int low_difference_counter;
+
     std::function<void(const unsigned int *, unsigned int)> get_digital_samples;
-    float freq_mark, freq_space;
-    static constexpr float fs = 48000.0f;
-
-    float phase_mark = 0.0f;
-    float phase_space = 0.0f;
-    float lp_filter_state = 0.0f;
-
-    const float alpha = 0.05f;
 };
 
 class V21_TX
 {
 public:
-    V21_TX(float omega_mark, float omega_space) : omega_mark(omega_mark), omega_space(omega_space), phase(0.f) {};
+    V21_TX(float omega_mark, float omega_space) :omega_mark(omega_mark),omega_space(omega_space),phase(0.f) {};
     void modulate(const unsigned int *in_digital_samples, float *out_analog_samples, unsigned int n);
 private:
     float omega_mark, omega_space;
