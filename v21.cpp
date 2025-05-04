a792bdc74e50f598cc93b236a0990c54cea110a6
joaobizineli <114615682+joaobizineli@users.noreply.github.com>
2025-05-04 15:42:15 -0300

    Update v21.cpp

--- v21.cpp
+++ v21.cpp
@@ -49,15 +49,3 @@ void V21_TX::modulate(const unsigned int *in_digital_samples, float *out_analog_
         phase = std::remainder(phase, 2 * std::numbers::pi);
     }
 }
-
-
-void V21_TX::modulate(const unsigned int *in_digital_samples, float *out_analog_samples, unsigned int n)
-{
-    while (n--) {
-        *out_analog_samples++ = sin(phase);
-        phase += (*in_digital_samples++ ? omega_mark : omega_space) * SAMPLING_PERIOD;
-
-        // evita que phase cresça indefinidamente, o que causaria perda de precisão
-        phase = remainder(phase, 2*std::numbers::pi);
-    }
-}
