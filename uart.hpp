cfbb4e0a12de1acec53388464fcce13362b1ebfb
joao_andrade <106917014+JoaoP9735@users.noreply.github.com>
2025-05-04 15:23:09 -0300

    Update uart.hpp

--- uart.hpp
+++ uart.hpp
@@ -7,13 +7,21 @@
 #include <stdint.h>
 #include "config.hpp"
 
+
+
 class UART_RX
 {
 public:
-    UART_RX(std::function<void(uint8_t)> get_byte) :get_byte(get_byte) {}
+    UART_RX(std::function<void(uint8_t)> get_byte) : get_byte(get_byte) {}
     void put_samples(const unsigned int *buffer, unsigned int n);
+
 private:
     std::function<void(uint8_t)> get_byte;
+    float ring_buffer[SAMPLES_PER_SYMBOL + 1] = {};
+    unsigned int bit_counter = 0;
+    unsigned int bit_counter_noisy_tolerance = 0;
+    uint8_t received_byte = 0;
+    int count = 0;
 };
 
 class UART_TX
