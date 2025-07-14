ab5bec931eee58bbfe81c9ffb9d7f61af161cde2
joao_andrade <106917014+JoaoP9735@users.noreply.github.com>
2025-07-14 09:10:40 -0300

    Update v21.hpp

--- v21.hpp
+++ v21.hpp
@@ -9,28 +9,29 @@
 class V21_RX
 {
 public:
-    V21_RX(float omega_mark, float omega_space, std::function<void(const unsigned int *, unsigned int)> get_digital_samples);
+    V21_RX(float omega_m, float omega_s, std::function<void(const unsigned int *, unsigned int)> get_digital_samples);
     void demodulate(const float *in_analog_samples, unsigned int n);
 private:
-    float omega_mark = 2 * M_PI * 1650;
-    float omega_space = 2 * M_PI * 1850;
+    float omega_m = 2 * M_PI * 1650;
+    float omega_s = 2 * M_PI * 1850;
     
-    float rl_cos_space, rl_sin_space, r_cos_space, r_sin_space; 
-    float rl_cos_mark, rl_sin_mark, r_cos_mark, r_sin_mark; 
-    float lp_numerator[3];
-    float lp_denominator[3];
+    float lp_num[3];
+    float lp_den[3];
 
     std::deque<float> sample_buffer;
     float vspace_r_buffer, vspace_i_buffer;
     float vmark_r_buffer, vmark_i_buffer;
-    float raw_decision_buffer[2];
-    float filtered_decision_buffer[2];
-
+    float buffer_decisao[2];
+    float buffer_filtrada[2];
+    float R;
+    float fs;
+    float T;
+    int L; 
     enum {
         IDLE,
-        CARRIER_DETECTED
+        CARRIER
     } state;
-    unsigned int low_difference_counter;
+    unsigned int low_state;
 
     std::function<void(const unsigned int *, unsigned int)> get_digital_samples;
 };
@@ -38,10 +39,10 @@ private:
 class V21_TX
 {
 public:
-    V21_TX(float omega_mark, float omega_space) :omega_mark(omega_mark),omega_space(omega_space),phase(0.f) {};
+    V21_TX(float omega_m, float omega_s) :omega_m(omega_m),omega_s(omega_s),phase(0.f) {};
     void modulate(const unsigned int *in_digital_samples, float *out_analog_samples, unsigned int n);
 private:
-    float omega_mark, omega_space;
+    float omega_m, omega_s;
     float phase;
 };
 
