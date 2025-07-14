e16e04146a62eeed8d07f4dcde1134edd8b11f8a
joao_andrade <106917014+JoaoP9735@users.noreply.github.com>
2025-07-14 09:10:10 -0300

    Update v21.cpp

--- v21.cpp
+++ v21.cpp
@@ -2,110 +2,97 @@
 #include <numbers>
 #include "v21.hpp"
 
-constexpr float BANDPASS_SMOOTHING = 0.99;
+constexpr float r = 0.99;
 
-V21_RX::V21_RX(float omega_mark, float omega_space, std::function<void(const unsigned int *, unsigned int)> get_digital_samples) :
-    omega_mark(omega_mark),
-    omega_space(omega_space),
-    get_digital_samples(get_digital_samples) 
+V21_RX::V21_RX(float omega_m, float omega_s, std::function<void(const unsigned int *, unsigned int)> get_digital_samples) : omega_m(omega_m),
+                                                                                                                            omega_s(omega_s),
+                                                                                                                            get_digital_samples(get_digital_samples)
 {
     for (int i = 0; i < SAMPLES_PER_SYMBOL; i++)
         this->sample_buffer.push_front(0.f);
-    this->vspace_r_buffer = 0.f;
-    this->vspace_i_buffer = 0.f;
-    this->vmark_r_buffer = 0.f;
-    this->vmark_i_buffer = 0.f;
-    this->raw_decision_buffer[0] = 0.f;
-    this->raw_decision_buffer[1] = 0.f;
-    this->filtered_decision_buffer[0] = 0.f;
-    this->filtered_decision_buffer[1] = 0.f;
-    this->low_difference_counter = 0;
-
-    // Butterworth passa-baixa
-    this->lp_numerator[0] = 0.00844269f;
-    this->lp_numerator[1] = 0.01688539f;
-    this->lp_numerator[2] = 0.00844269f;
+    vspace_r_buffer = 0.f;
+    vspace_i_buffer = 0.f;
+    vmark_r_buffer = 0.f;
+    vmark_i_buffer = 0.f;
+    buffer_decisao[0] = 0.f;
+    buffer_decisao[1] = 0.f;
+    buffer_filtrada[0] = 0.f;
+    buffer_filtrada[1] = 0.f;
+    low_state = 0;
+    R = 30;
+    fs = 48000;
+    T = 1 / fs;
+    L = SAMPLES_PER_SYMBOL;
     
-    this->lp_denominator[0] = 1.0f;
-    this->lp_denominator[1] = -1.72377617f;
-    this->lp_denominator[2] = 0.75754694f;
-
-    this->rl_cos_space = pow(BANDPASS_SMOOTHING, SAMPLES_PER_SYMBOL) * cos(omega_space * SAMPLES_PER_SYMBOL * SAMPLING_PERIOD); 
-    this->rl_sin_space = pow(BANDPASS_SMOOTHING, SAMPLES_PER_SYMBOL) * sin(omega_space * SAMPLES_PER_SYMBOL * SAMPLING_PERIOD); 
-    this->r_cos_space = BANDPASS_SMOOTHING * cos(omega_space * SAMPLING_PERIOD); 
-    this->r_sin_space = BANDPASS_SMOOTHING * sin(omega_space * SAMPLING_PERIOD); 
-
-    this->rl_cos_mark = pow(BANDPASS_SMOOTHING, SAMPLES_PER_SYMBOL) * cos(omega_mark * SAMPLES_PER_SYMBOL * SAMPLING_PERIOD); 
-    this->rl_sin_mark = pow(BANDPASS_SMOOTHING, SAMPLES_PER_SYMBOL) * sin(omega_mark * SAMPLES_PER_SYMBOL * SAMPLING_PERIOD); 
-    this->r_cos_mark = BANDPASS_SMOOTHING * cos(omega_mark * SAMPLING_PERIOD); 
-    this->r_sin_mark = BANDPASS_SMOOTHING * sin(omega_mark * SAMPLING_PERIOD); 
+    this->lp_num[0] = 0.0028982f;   
+    this->lp_num[1] = 0.0057964f;   
+    this->lp_num[2] = 0.0028982f;   
+    this->lp_den[0] = 1.0f;         
+    this->lp_den[1] = -1.8668923f;  
+    this->lp_den[2] = 0.8752146f;   
 };
 
 void V21_RX::demodulate(const float *in_analog_samples, unsigned int n)
 {
     unsigned int digital_samples[n];
-    const int L = SAMPLES_PER_SYMBOL;
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         this->sample_buffer.push_front(in_analog_samples[i]);
 
-        float vspace_r = this->sample_buffer[0] - this->rl_cos_space * this->sample_buffer[L]
-                            + this->r_cos_space * this->vspace_r_buffer - this->r_sin_space * vspace_i_buffer;
-        float vspace_i = -this->rl_sin_space * this->sample_buffer[L] + this->r_cos_space * vspace_i_buffer
-                            + this->r_sin_space * vspace_r_buffer;
-        float vmark_r = this->sample_buffer[0] - this->rl_cos_mark * this->sample_buffer[L]
-                            + this->r_cos_mark * this->vmark_r_buffer - this->r_sin_mark * vmark_i_buffer;
-        float vmark_i = -this->rl_sin_mark * this->sample_buffer[L] + this->r_cos_mark * vmark_i_buffer
-                            + this->r_sin_mark * vmark_r_buffer;
+        float vspace_r = sample_buffer[0] - pow(r, L) * cos(omega_s * L * T) * sample_buffer[L] + r * cos(omega_s * T) * vspace_r_buffer - r * sin(omega_s * T) * vspace_i_buffer;
+        float vspace_i = -pow(r, L) * sin(omega_s * L * T) * sample_buffer[L] + r * cos(omega_s * T) * vspace_i_buffer + r * sin(omega_s * T) * vspace_r_buffer;
+        float vmark_r = sample_buffer[0] - pow(r, L) * cos(omega_m * L * T) * sample_buffer[L] + r * cos(omega_m * T) * vmark_r_buffer - r * sin(omega_m * T) * vmark_i_buffer;
+        float vmark_i = -pow(r, L) * sin(omega_m * L * T) * sample_buffer[L] + r * cos(omega_m * T) * vmark_i_buffer + r * sin(omega_m * T) * vmark_r_buffer;
 
-        float raw_decision = vmark_r * vmark_r + vmark_i * vmark_i -
-                                vspace_r * vspace_r - vspace_i * vspace_i; 
-        float filtered_decision = 
-            this->lp_numerator[0] * raw_decision
-            + this->lp_numerator[1] * this->raw_decision_buffer[0]
-            + this->lp_numerator[2] * this->raw_decision_buffer[1]
-            - this->lp_denominator[1] * this->filtered_decision_buffer[0]
-            - this->lp_denominator[2] * this->filtered_decision_buffer[1];
-        filtered_decision /= this->lp_denominator[0];
+        float decisao = vmark_r * vmark_r + vmark_i * vmark_i - vspace_r * vspace_r - vspace_i * vspace_i;
 
-        switch (this->state) {
-            case IDLE:
-                if (abs(filtered_decision) > 120) {
-                    digital_samples[i] = filtered_decision > 0 ? 1 : 0;
-                    this->low_difference_counter = 0;
-                    this->state = CARRIER_DETECTED;
-                } else
-                    digital_samples[i] = 1;
+        float decisao_filtro =
+            (lp_num[0] * decisao + lp_num[1] * buffer_decisao[0] + lp_num[2] * buffer_decisao[1] - lp_den[1] * buffer_filtrada[0] - lp_den[2] * buffer_filtrada[1]) / lp_den[0];
 
-                break;
+        switch (state)
+        {
+        case IDLE:
+            if (abs(decisao_filtro) > 120)
+            {
+                digital_samples[i] = decisao_filtro > 0 ? 1 : 0;
+                low_state = 0;
+                this->state = CARRIER;
+            }
+            else
+                digital_samples[i] = 1;
 
-            case CARRIER_DETECTED:
-                if (abs(filtered_decision) < 60)
-                    this->low_difference_counter++;
-                else
-                    this->low_difference_counter = 0;
+            break;
 
-                if (this->low_difference_counter >= 50) {
-                    digital_samples[i] = 1;
-                    this->state = IDLE;
-                } else
-                    digital_samples[i] = filtered_decision > 0 ? 1 : 0;
+        case CARRIER:
+            if (abs(decisao_filtro) < 60)
+                low_state++;
+            else
+                low_state = 0;
 
-                break;
+            if (low_state >= 50)
+            {
+                digital_samples[i] = 1;
+                state = IDLE;
+            }
+            else
+                digital_samples[i] = decisao_filtro > 0 ? 1 : 0;
 
-            default: break;
+            break;
+
+        default:
+            break;
         }
 
-        // Updating buffers 
         this->sample_buffer.pop_back();
         this->vspace_r_buffer = vspace_r;
         this->vspace_i_buffer = vspace_i;
         this->vmark_r_buffer = vmark_r;
         this->vmark_i_buffer = vmark_i;
-        this->raw_decision_buffer[1] = this->raw_decision_buffer[0];
-        this->raw_decision_buffer[0] = raw_decision;
-        this->filtered_decision_buffer[1] = this->filtered_decision_buffer[0];
-        this->filtered_decision_buffer[0] = filtered_decision;
+        this->buffer_decisao[1] = this->buffer_decisao[0];
+        this->buffer_decisao[0] = decisao;
+        this->buffer_filtrada[1] = this->buffer_filtrada[0];
+        this->buffer_filtrada[0] = decisao_filtro;
     }
 
     get_digital_samples(digital_samples, n);
@@ -113,9 +100,12 @@ void V21_RX::demodulate(const float *in_analog_samples, unsigned int n)
 
 void V21_TX::modulate(const unsigned int *in_digital_samples, float *out_analog_samples, unsigned int n)
 {
-    while (n--) {
+    while (n--)
+    {
         *out_analog_samples++ = sin(phase);
-        phase += (*in_digital_samples++ ? omega_mark : omega_space) * SAMPLING_PERIOD;
-        phase = remainder(phase, 2*M_PI);
+        phase += (*in_digital_samples++ ? omega_m : omega_s) * SAMPLING_PERIOD;
+
+        // evita que phase cresça indefinidamente, o que causaria perda de precisão
+        phase = remainder(phase, 2 * M_PI);
     }
 }
